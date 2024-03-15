cfd46e9363ab8e5e35228d54096bc02d933acac2
GoncaloGracioso <202300241@estudantes.ips.pt>
2024-03-15 10:10:30 +0000

    nivel 2

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -10,6 +10,7 @@ public class WordGuessingGame
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
+    private InputReader reader;
 
     /**
      * Constructor for objects of class WordGuessingGame
@@ -19,10 +20,44 @@ public class WordGuessingGame
         this.hiddenWord = "abc";
         this.guessedWord = "___";
         this.numberOfTries = 0;
+        this.reader = new InputReader();
     }
     
-    public void showGuessedWord(){
+    private void showGuessedWord(){
         System.out.println(guessedWord);
+    }
+    
+    public void play(){
+        char answer;
+        do{
+            System.out.println(guessedWord);
+            answer = reader.getChar("Guess char\n");
+            guess(answer);
+            numberOfTries++;
+        }while(!guessedWord.equals(hiddenWord));
+        showResult();
+        numberOfTries = 0;
+    }
+    
+    private void showWelcome(){
+        System.out.println("Welcome");
+    }
+    
+    private void guess(char answer){
+        char[] arr1 = hiddenWord.toCharArray();
+        char[] arr2 = guessedWord.toCharArray();
+        for(int i = 0; i < arr1.length; i++){
+            if(arr1[i] == answer){
+                arr2[i] = arr1[i];
+            }
+        }
+        guessedWord = new String(arr2);
+    }
+    
+    private void showResult(){
+        if(guessedWord.equals(hiddenWord)){
+            System.out.println(numberOfTries);
+        }
     }
     
     public String getHiddenWord(){
